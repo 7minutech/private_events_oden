e73cdc8f7b7a94c70cb41215e0a8fc26320abbe9
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-09 13:36:05 -0500

    feat: generated user model w/ devise & migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_09_172250) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_09_182935) do
   create_table "events", force: :cascade do |t|
     t.date "date"
     t.string "location", default: "TBD", null: false
@@ -18,5 +18,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_09_172250) do
     t.datetime "updated_at", null: false
     t.string "name", default: "TBD", null: false
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
