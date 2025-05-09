1262af41d6189387a0268f813a2b08932de1a4bb
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-09 11:59:09 -0500

    feat: name & description cols for events

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_09_164050) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_09_165628) do
   create_table "events", force: :cascade do |t|
     t.date "date"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
   end
 end
