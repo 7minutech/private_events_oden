e4e062a81c80c3385a343dcab8cae6dc95a00d31
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-09 15:03:02 -0500

    feat: association b/w event creator and the event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_09_182935) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_09_195755) do
   create_table "events", force: :cascade do |t|
     t.date "date"
     t.string "location", default: "TBD", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_09_182935) do
     t.datetime "updated_at", null: false
     t.string "name", default: "TBD", null: false
     t.text "description"
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_09_182935) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
