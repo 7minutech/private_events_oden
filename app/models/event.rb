391b7147160b9e0462a3b049d4abaf6b2696b29f
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-10 14:20:01 -0500

    feat: past & upcoming event class methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :bookings, foreign_key: :attended_event
   has_many :attendees, through: :bookings
+
+  def self.past
+    Event.where("date < ?", Date.today)
+  end
+
+  def self.upcoming
+    Event.where("date > ?", Date.today).or(Event.where(date: nil))
+  end
 end
