bb24901a0909259d62810625a62f2232905fa9b4
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-09 21:12:26 -0500

    feat: require log in for user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  before_action :require_login
+
   def show
     @user = current_user
     @events = @user.created_events
+  end
+
+  private
+
+  def require_login
+    redirect_to new_user_session_path if !user_signed_in?
   end
 end
