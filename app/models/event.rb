94e722754f262bc38cf4634e15b06aa985cbbf16
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-10 14:55:45 -0500

    feat: depenedent destory for for event bookings

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
-  has_many :bookings, foreign_key: :attended_event
+  has_many :bookings, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :bookings
 
   scope :past, -> { where("date < ?", Date.today) }
