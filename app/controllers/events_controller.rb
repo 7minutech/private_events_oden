c4d50a01731727115a7480204a603773d14245f6
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-09 20:37:35 -0500

    feat: event create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,19 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def create
+    @user = current_user
+    @event = @user.created_events.build(user_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:event).permit(:name, :description, :date, :location)
+  end
 end
