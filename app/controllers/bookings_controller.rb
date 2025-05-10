e5423eb5e27750558da90d16f4a1f30f2961c01e
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-10 12:42:00 -0500

    feat: bookings controller create action w/ helpers
    Should check if user has already chosen to attend
    an event w/ helpers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,40 @@ class BookingsController < ApplicationController
     @event = Event.find(params[:attended_event_id])
     @booking = Booking.new
   end
+
+  def create
+    @user = current_user
+    @event = Event.find(params[:booking][:attended_event_id].to_i)
+    @booking = @user.bookings.build(attended_event: @event)
+    if !already_attending? && @booking.save
+      redirect_to user_path(@user)
+    else
+      if already_attending?
+        flash.now[:alert] = "Already Attending"
+      else
+        flash.now[:alert] = "Error While Booking"
+      end
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def already_attending?
+    @user = current_user
+    event_id = (params[:booking][:attended_event_id].to_i)
+    attending_events[event_id] > 1
+  end
+
+  def attending_events
+    attending_events = {}
+    @user.bookings.each do |booking|
+      if attending_events[booking.attended_event_id].nil?
+        attending_events[booking.attended_event_id] = 1
+      else
+        attending_events[booking.attended_event_id] += 1
+      end
+    end
+    attending_events
+  end
 end
